95d6f6fad9f0d4b976b99675de2d6a46d49621b0
jessicafu232 <88943244+jessicafu232@users.noreply.github.com>
2023-01-18 12:22:53 -0600

    Added automation for web browser, updated formatting and features

--- costing_data_script_no_browser.py
+++ costing_data_script_no_browser.py
@@ -8,15 +8,6 @@ import math
 import webbrowser
 import subprocess
 
-# Webdriver
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from datetime import date
 import time
 import glob
@@ -38,87 +29,6 @@ root = tree.getroot()
 # Checking the most recent year/months in the file ------------------------------------------------------
 most_recent_year = root[1].attrib['year']
 most_recent_month = root[1].attrib['month']
-
-month_dict = {'January': '1', 'February': '2', 'March': '3', 'April': '4', 'May': '5', 'June': '6', 'July': '7', 'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12'}
-most_recent_month_num = month_dict[most_recent_month]
-
-# Downloading the most recent data file! ---------------------------------------------------------------------------------------------------------------------
-
-# Opening a Chrome tab
-driver = webdriver.Chrome()
-driver.get('https://chemengonline.com/pci')
-
-# Logging in
-button = driver.find_element(By.LINK_TEXT, "Login")
-driver.implicitly_wait(5)
-ActionChains(driver).move_to_element(button).click(button).perform()
-
-login_form = driver.find_element(By.NAME, "log")
-login_form.clear()
-login_form.send_keys("whitneyg@chemstations.com")
-
-password_form = driver.find_element(By.NAME, "pwd")
-password_form.clear()
-password_form.send_keys("CHEMCADNXTis1")
-
-password_form.submit()
-
-# Filling in the download form
-download_form = driver.find_element(By.XPATH, "/html/body/div[7]/div/div[1]/table/tbody/tr/td[2]/table/tbody/tr[1]/td/form")
-#all_options = download_form.find_elements(By.TAG_NAME, "option")
-month_start_option = Select(download_form.find_element(By.NAME, "From_Month"))
-month_start_option.select_by_value(most_recent_month_num) 
-
-year_start_option = Select(download_form.find_element(By.NAME, "From_Year"))
-year_start_option.select_by_value(most_recent_year)
-
-month_end_option = Select(download_form.find_element(By.NAME, "To_Month"))
-month_end_option.select_by_value('12')
-
-year_end_option = Select(download_form.find_element(By.NAME, "To_Year"))
-
-# Trying the current year. If the most recent year doesn't work (i.e. the data hasn't caught up yet), it's going to be this year - 1
-year = int(str(date.today())[0:4])
-
-try:
-	year_end_option.select_by_value(str(year))
-except:
-	year_end_option.select_by_value(str(year-1))
-
-wait = WebDriverWait(driver, 10)
-original_window = driver.current_window_handle
-assert len(driver.window_handles) == 1
-download_form.submit()
-
-# Waiting until the popup opens!
-wait.until(EC.number_of_windows_to_be(2))
-for window_handle in driver.window_handles:
-        if window_handle != original_window:
-            driver.switch_to.window(window_handle)
-            break
-
-# Clicking download
-link_elem = driver.find_element(By.XPATH, "/html/body/p/a")
-my_link = link_elem.get_attribute('href')
-
-ActionChains(driver).move_to_element(link_elem).click(link_elem).perform()
-
-# Closing web driver!
-time.sleep(5)
-driver.close()
-if driver:
-	driver.quit()
-
-# Finding the latest file in the download folder
-list_of_files = glob.glob('../../../Downloads/*') # * means all if need specific format then *.csv
-latest_file = max(list_of_files, key=os.path.getctime)
-
-# Getting filename
-a = str(latest_file).split('\\')[1]
-
-# Moving the file to the data folder in the computer
-os.rename(latest_file, 'data/' + a)
-
 
 # Converting data into excel file ---------------------------------------------------------------------------------------------------------------------------------------
 # This portion is necessary because the raw data saved from the CEPCI website isn't actually an excel file, it's a text file with a .xls extension (for some reason)
@@ -142,7 +52,6 @@ for i in range(len(data)):
 
 book.save('excel_data' + '.xls')
 f.close()
-
 
 # Reading excel file -----------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -288,5 +197,5 @@ elif user_response == 'n':
 	print("Saving the file as " + args.filename + ".xml. The old costing_data.xml file will not be overriden.")
 
 print("Saved! Closing program...")
-print("If the program does not close after 30 seconds, press Ctrl+C to exit. This will not cause any issues.")
+#print("If the program does not close after 30 seconds, press Ctrl+C to exit. This will not cause any issues.")
 quit()
